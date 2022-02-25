95641574f28ca32ba646990683fa7106a206d49e
Aniksen1 <anikmathart@gmail.com>
2022-02-24 22:43:34 -0500

    single commit

--- src/NumberOfWordInString.java
+++ src/NumberOfWordInString.java
@@ -2,7 +2,9 @@
 public class NumberOfWordInString {
 
 	public static void main(String[] args) {
-		
+		String sentence="I am a QA Engineer";
+		int numberOfWord=sentence.split(" ").length;
+		System.out.println(numberOfWord);
 
 	}
 
