c773ae8c12b0da5120cf80eee7e93b71e71f9046
Aniksen1 <anikmathart@gmail.com>
2022-03-05 16:12:18 -0500

    Vowel Update

--- src/VowelConsonant.java
+++ src/VowelConsonant.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class VowelConsonant {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Please, give a Alphabet input");
 		Scanner sc=new Scanner(System.in);
 		char input=sc.next().charAt(0);
 		
 		if('a'<=input && input<='z'||'A'<=input && input<='Z') {
-		if(input==('a')||input==('e')||input==('i')||input==('o')||input==('u')) {
+		if(input==('a')||input==('e')||input==('i')||input==('o')||input==('u')||input==('A')||input==('E')||input==('I')||input==('O')||input==('U')) {
 			System.out.println(input+" is a Vowel");
 		}
 		else {
